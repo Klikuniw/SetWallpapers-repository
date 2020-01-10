5f261d2a346a8937daaa1847dc0a4d6f00800ad7
Igor Klikun <38037567+Klikuniw@users.noreply.github.com>
2020-01-11 00:14:20 +0200

    Add IWallpaperParser and WallpaperCraftParser class. Wrote propertiew and methods of the interface

---
+++ SetWallpapers/SetWallpapers/Model/IWallpaperParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetWallpapers.Model
+{
+    interface IWallpaperParser
+    {
+        string WebsiteName { get; }
+
+        List<string> Categories { get; }
+        List<string> Resolutions { get; }
+        
+        string ParseImage(string path);
+        List<string> ParseImages(List<string> paths);
+
+        List<string> ReadCategories(string paths);
+        List<string> ReadResolutions(string paths);
+
+    }
+}
---
+++ SetWallpapers/SetWallpapers/Model/WallpaperCraftParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetWallpapers.Model
+{
+    public class WallpaperCraftParser : IWallpaperParser
+    {
+        public string WebsiteName => "";
+
+        public List<string> Categories => ReadCategories("");
+
+        public List<string> Resolutions => ReadResolutions("");
+
+        public string ParseImage(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ParseImages(List<string> paths)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ReadCategories(string paths)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ReadResolutions(string paths)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string LoadDocument(string path)
+        {
+            string htmlDoc = "";
+
+            return htmlDoc;
+        }
+
+
+    }
+}
