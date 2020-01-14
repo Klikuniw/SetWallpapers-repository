7b5fead9179f2abcca6b4ca6bcc10a881941ef8f
Igor Klikun <38037567+Klikuniw@users.noreply.github.com>
2020-01-14 11:28:16 +0200

    Wrote categories binding

--- SetWallpapers/SetWallpapers/Model/Category.cs
+++ SetWallpapers/SetWallpapers/Model/Category.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SetWallpapers.Model
 {
-    public class Category
+    public class Category : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Tag { get; set; }
-        public bool Checked { get; set; }
+        private string _name;
+        private string _tag;
+        private bool _checked;
 
+        public string Name
+        {
+            get => _name;
+            set
+            { 
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Tag
+        {
+            get => _tag;
+            set
+            {
+                _tag = value;
+                OnPropertyChanged();
+            } 
+        }
+        public bool Checked
+        {
+            get => _checked;
+            set
+            { 
+                _checked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
