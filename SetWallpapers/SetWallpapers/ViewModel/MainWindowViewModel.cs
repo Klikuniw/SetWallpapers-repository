fa36993b6343366aaeddb40642e6446f596540f6
Igor Klikun <38037567+Klikuniw@users.noreply.github.com>
2020-01-17 12:41:09 +0200

    Add timers

--- SetWallpapers/SetWallpapers/ViewModel/MainWindowViewModel.cs
+++ SetWallpapers/SetWallpapers/ViewModel/MainWindowViewModel.cs
@@ -9,6 +9,8 @@ using SetWallpapers.Model;
 using System.Windows.Input;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Threading;
+
 namespace SetWallpapers.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
@@ -19,6 +21,10 @@ namespace SetWallpapers.ViewModel
         private ObservableCollection<string> _intervals;
         private Resolution _selectedResolution;
         private string _selectedInterval;
+        private TimeSpan _time;
+
+        private readonly DispatcherTimer _dispatcherTimerShowTime = new DispatcherTimer();
+        private readonly DispatcherTimer _dispatcherTimerExecuteMethod = new DispatcherTimer();
 
         private ICommand _saveChangesCommand;
         private ICommand _getResolutionCommand;
@@ -83,7 +89,7 @@ namespace SetWallpapers.ViewModel
             {
                 if (_intervals == null)
                 {
-                    _intervals = new ObservableCollection<string>() { "5 min", "10 min", "1 day" };
+                    _intervals = new ObservableCollection<string>() { "5 sec","5 min", "10 min", "1 day" };
                 }
 
                 return _intervals;
@@ -123,7 +129,18 @@ namespace SetWallpapers.ViewModel
                 OnPropertyChanged("SelectedInterval");
             }
         }
-
+        public TimeSpan Time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                _time = value;
+                OnPropertyChanged("Time");
+            }
+        }
 
         private void ExecuteGetResolutionCommand(object obj)
         {
@@ -132,8 +149,64 @@ namespace SetWallpapers.ViewModel
         private void ExecuteSaveChangesCommand(object obj)
         {
             _parser.SaveChanges("wallpaperscraftInfo.xml", Categories.ToList(), SelectedResolution, SelectedInterval);
+
+            _dispatcherTimerShowTime.Tick += new EventHandler(dispatcherTimer_Tick);
+            _dispatcherTimerShowTime.Interval = new TimeSpan(0,0,0,1);
+            
+            _dispatcherTimerExecuteMethod.Tick+= new EventHandler(Show);
+            _dispatcherTimerExecuteMethod.Interval = ToTimeSpan(SelectedInterval);
+
+
+
+            Time = _dispatcherTimerExecuteMethod.Interval;
+            _dispatcherTimerShowTime.Start();
+            _dispatcherTimerExecuteMethod.Start();
+
         }
 
+        private void Show(object sender, EventArgs e)
+        {
+            MessageBox.Show("sdasd");
+        }
 
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (Time.Equals(new TimeSpan(0,0,0)))
+            {
+                Time = _dispatcherTimerExecuteMethod.Interval;
+            }
+            else
+            {
+                Time = new TimeSpan(Time.Hours,Time.Minutes,Time.Seconds-1);
+            }
+            
+        }
+
+        private TimeSpan ToTimeSpan(string time)
+        {
+            TimeSpan res = new TimeSpan(0, 0, 0);
+
+            var value = SelectedInterval.Split(' ');
+            switch (value[1])
+            {
+                case "min":
+                {
+                    res = new TimeSpan(0, Convert.ToInt32(value[0]),0);
+                    break;
+                }
+                case "day":
+                {
+                    res = new TimeSpan(Convert.ToInt32(value[0]),0,0, 0);
+                        break;
+                }
+                case "sec":
+                {
+                    res = new TimeSpan(0, 0, 0, Convert.ToInt32(value[0]));
+                    break;
+                }
+            }
+
+            return res;
+        }
     }
 }
