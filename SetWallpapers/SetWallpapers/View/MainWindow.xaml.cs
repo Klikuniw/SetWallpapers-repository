fb0c6b66a66488c804cbf6b9be5a53d3678d4c69
Igor Klikun <38037567+Klikuniw@users.noreply.github.com>
2020-01-17 11:55:14 +0200

    Add hide to tray

--- SetWallpapers/SetWallpapers/View/MainWindow.xaml.cs
+++ SetWallpapers/SetWallpapers/View/MainWindow.xaml.cs
@@ -23,6 +23,23 @@ namespace SetWallpapers
         public MainWindow()
         {
             InitializeComponent();
+
+            System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
+            ni.Icon = new System.Drawing.Icon(@"wallpaper_104165.ico");
+            ni.Visible = true;
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+        }
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == System.Windows.WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
         }
     }
 }
